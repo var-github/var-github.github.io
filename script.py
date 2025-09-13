98e6fb0c6543140aee56a165a11f4e8a8bab4548
Varun R <64779815+var-github@users.noreply.github.com>
2025-09-13 18:43:23 +0530

    Update script.py

--- script.py
+++ script.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     urls = eval(sys.argv[1]) if len(sys.argv) > 1 else []
 
     with open('projects_to_display.json', 'r') as file:
-        data = json.load(file)
+        data = list(json.load(file).values())
         print(data)
     
     with ThreadPoolExecutor(max_workers=len(urls)) as executor:
